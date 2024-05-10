a1de9bc73e4f6b9ae963f24a24fbe2cddcf2634a
voltarou <bagusadnan04@gmail.com>
2024-05-10 10:38:50 +0800

    int push

--- UcupCina1998/UcupCina1998.cpp
+++ UcupCina1998/UcupCina1998.cpp
@@ -22,8 +22,13 @@ public:
 };
 
 int push(int value) {
+    Node* top;
     Node* newNode = new Node();
     newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+    cout << "Push value:" << value << endl;
+    return value;
 }
 int main()
 {
