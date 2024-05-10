406bdcfef9a2f9eb3ccb9ccd77f198ef836ea81f
voltarou <bagusadnan04@gmail.com>
2024-05-10 11:11:54 +0800

    int main

--- UcupCina1998/UcupCina1998.cpp
+++ UcupCina1998/UcupCina1998.cpp
@@ -13,58 +13,65 @@ public :
 };
 
 class stack {
-private :
+private:
     Node* top;
 
 public:
     stack() {
         top = NULL;
     }
+
+
+    int push(int value) {
+        Node* top;
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value:" << value << endl;
+        return value;
+    }
+
+    void pop() {
+        Node* top;
+
+        if (isEmpty()) {
+            cout << "stack is empty." << endl;
+        }
+        cout << "Popped value:" << top->data << endl;
+        top = top->next;
+    }
+
+    void peek() {
+        Node* top;
+        if (top == NULL) {
+            cout << "List is empty." << endl;
+        }
+        else {
+            Node* current = top;
+            while (current != NULL) {
+                cout << current->data << "" << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+    bool isEmpty() {
+        Node* top;
+        return top == NULL;
+    }
+
 };
 
-int push(int value) {
-    Node* top;
-    Node* newNode = new Node();
-    newNode->data = value;
-    newNode->next = top;
-    top = newNode;
-    cout << "Push value:" << value << endl;
-    return value;
-}
 
-void pop() {
-    Node* top;
-    
-    if (isEmpty()){
-        cout << "stack is empty." << endl;
-    }
-    cout << "Popped value:" << top->data << endl;
-    top = top->next;
-}
-
-void peek() {
-    Node* top;
-    if (top == NULL) {
-        cout << "List is empty." << endl;
-    }
-    else {
-        Node* current = top;
-        while (current != NULL) {
-            cout << current->data << "" << endl;
-            current = current->next;
-        }
-        cout << endl;
-    }
-}
-
-bool isEmpty() {
-    Node* top;
-    return top == NULL;
-}
 
 int main()
 {
-    
+    stack stack;
+
+    int choice = 0;
+    int value;
 }
 
  
